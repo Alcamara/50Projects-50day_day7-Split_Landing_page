2389a742fa6761d9f44a0b0c217f83d7028cbbcf
Al Camara <Alhousseinec@gmail.com>
2022-08-28 21:17:02 -0500

    added hover effects

--- client.js
+++ client.js
@@ -1,6 +1,26 @@
 $(document).ready(readyNow);
 
 
-function readyNow(params) {
+function readyNow() {
     console.log('it works');
+
+    $('.split.left').on('mouseenter', incrWidthLeft).on('mouseleave', decrWidthLeft);
+
+    $('.split.right').on('mouseenter', incrWidthRight).on('mouseleave', decrWidthRight);
+}
+
+function incrWidthLeft(event) {
+    console.log(event.currentTarget);
+   $('.container').addClass('hover-left')
+}
+function decrWidthLeft(){
+    $('.container').removeClass('hover-left')
+}
+
+function incrWidthRight(event) {
+    console.log(event.currentTarget);
+   $('.container').addClass('hover-right')
+}
+function decrWidthRight(){
+    $('.container').removeClass('hover-right')
 }
